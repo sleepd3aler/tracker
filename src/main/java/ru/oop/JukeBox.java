713fc4da1bafc4f2cc6fd8e1103ff21e1db4c5f5
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-05 13:11:31 +0300

    7.4 Вызов метода с аргументами

---
+++ src/main/java/ru/oop/JukeBox.java
@@ -0,0 +1,16 @@
+package ru.oop;
+
+public class JukeBox {
+    public void music(int position) {
+        switch (position) {
+            case 1 -> System.out.println("Пусть бегут неуклюже");
+            case 2 -> System.out.println("Спокойной ночи");
+            default -> System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        JukeBox song = new JukeBox();
+        song.music(2);
+    }
+}
