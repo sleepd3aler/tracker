cd515a8e239554dd6397302dfd7993087b22ed99
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-05 12:23:59 +0300

    7.2 Создание обьекта

---
+++ src/main/java/ru/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog zhychka = new Dog();
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+    }
+}
