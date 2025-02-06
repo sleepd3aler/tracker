d1813397458132124dcba6a0138390875c0eb99f
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-06 17:50:44 +0300

    7.9 Перегрузка конструктора

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -4,18 +4,6 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
@@ -29,8 +17,20 @@ public class Item {
 
     }
 
-        public void setName(String name) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    }
+}
