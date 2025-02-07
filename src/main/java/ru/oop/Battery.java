96e454dd2473258d71ff2a8fefb34dd10af05d76
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-07 14:59:07 +0300

    7.11 Класс использующий сам себя

--- src/main/java/ru/oop/Battery.java
+++ src/main/java/ru/oop/Battery.java
@@ -13,7 +13,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-       another.load = another.load + this.load;
+       another.load += this.load;
        this.load = 0;
 
     }
