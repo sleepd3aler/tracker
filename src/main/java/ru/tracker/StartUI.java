6d26337608f53e63101aafa3ea2708e5100be38a
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-17 21:07:55 +0300

    8.7 Date. Отображение даты

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -7,7 +7,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         LocalDateTime time = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MMMM.yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MMMM.EEEE.yyyy HH:mm:ss");
         String dateTime = time.format(formatter);
         System.out.println(dateTime);
     }
