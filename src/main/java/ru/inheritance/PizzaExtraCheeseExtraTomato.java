7a34bf8e1553b5db34317c7b39f22dc1dfba67d0
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-17 17:22:59 +0300

    8.5 Вызов переопределенного метода родителя super method

---
+++ src/main/java/ru/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,8 @@
+package ru.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
+    @Override
+    public String name() {
+        return super.name() + " + extra tomato";
+    }
+}
