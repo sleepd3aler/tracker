105cff7c097ccbd26834b53f0ec48eae79c21084
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-18 20:53:49 +0300

    9.3 Массивы и модели

--- src/main/java/ru/pojo/Library.java
+++ src/main/java/ru/pojo/Library.java
@@ -2,16 +2,9 @@ package ru.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book = new Book("Clean Code", 500);
-        Book book2 = new Book("Dracula", 670);
-        Book book3 = new Book("Tom Soyer", 700);
-        Book book4 = new Book("Bartenders Bible", 350);
 
-        Book[] books = new Book[4];
-        books[0] = book;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
+        Book[] books = {new Book("Clean Code", 500), new Book("Dracula", 670),
+                new Book("Tom Soyer", 700), new Book("Bartenders Bible", 350)};
 
         for (Book bks : books) {
             System.out.println("Book " + "\"" + bks.getName() + "\"" + " contains : "
@@ -27,7 +20,7 @@ public class Library {
 
         System.out.println("Show only Clean Code");
         for (Book bk : books) {
-            if (bk.getName().equals("Clean Code")) {
+            if ("Clean Code".equals(bk.getName())) {
                 System.out.println("Requested book " + "\"" + bk.getName() + "\"" + " has founded, and contains: "
                         + bk.getCount() + " pages");
             }
