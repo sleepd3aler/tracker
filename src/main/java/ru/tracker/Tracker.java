415a46d8144b1d077dba61d8309119cb83b447e6
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-25 01:26:55 +0300

    9.8 Метод замены заявки Tracker.replace

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -32,14 +32,28 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        return items[indexOf(id)];
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
     }
+
+    public boolean replace(int id, Item item) {
+        int current = indexOf(id);
+        if (current != -1) {
+            item.setId(id);
+            items[current] = item;
+            return true;
+        }
+        return false;
+    }
+
 }
