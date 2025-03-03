f00ed0371e3b011ac1f0ef9e6acf3b65c86ff75d
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-03 21:05:28 +0300

    10.2 Класс Scanner

---
+++ src/main/java/ru/io/MagicBall.java
@@ -0,0 +1,20 @@
+package ru.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        String input = scanner.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> {
+                System.out.println("Может быть");
+            }
+        }
+    }
+}
