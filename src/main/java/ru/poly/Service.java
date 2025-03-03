811717969b9b7e26daac2e588637b72e36c7ee80
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-03 20:55:23 +0300

    10.1 Полиморфизм

---
+++ src/main/java/ru/poly/Service.java
@@ -0,0 +1,21 @@
+package ru.poly;
+
+public class Service {
+    private Store store;
+
+    public Service(Store store) {
+        this.store = store;
+    }
+
+    public void add() {
+        store.save("Ivan Ivanov");
+    }
+
+    public static void main(String[] args) {
+        FileStore store = new FileStore();
+      /*  MemStore store = new MemStore(); */
+        Service service = new Service(store);
+        service.add();
+
+    }
+}
