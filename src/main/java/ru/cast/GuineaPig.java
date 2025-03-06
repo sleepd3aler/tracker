f28a2a8037c31934c795a10010e92d84ac1a704f
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-07 01:07:28 +0300

    10.15 Зачем нужно приведение типов

---
+++ src/main/java/ru/cast/GuineaPig.java
@@ -0,0 +1,8 @@
+package ru.cast;
+
+public class GuineaPig implements Animal {
+    @Override
+    public void sound() {
+        System.out.println(getClass().getSimpleName() + " громко пищит, для привлечения внимания.");
+    }
+}
