82f0ec09fc90efed61bada1c280243a804acf3df
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-07 10:55:49 +0300

    10.11 Interface

---
+++ src/main/java/ru/polymorph/TownCar.java
@@ -0,0 +1,30 @@
+package ru.polymorph;
+
+public class TownCar implements Vehicle {
+
+    @Override
+    public void changeGear() {
+        System.out.println("Коробка передач автомат. Поставить селектор в режим D.");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("Нажатие на педаль газа механически открывает дроссельную заслонку");
+    }
+
+    @Override
+    public void steer() {
+        System.out.println("Стандартное рулевое управление");
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("Стандартная тормозная система");
+    }
+
+    @Override
+    public void refill() {
+        System.out.println("Заправить 60л бензина");
+    }
+
+}
