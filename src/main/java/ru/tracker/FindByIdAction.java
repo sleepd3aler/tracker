be2847644f29321a6e080e92e07726e18fe056b6
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-11 00:22:14 +0300

    10.23 Реализация меню за счет шаблона стратегия

---
+++ src/main/java/ru/tracker/FindByIdAction.java
@@ -0,0 +1,21 @@
+package ru.tracker;
+
+public class FindByIdAction implements UserAction {
+    @Override
+    public String name() {
+        return "Показать заявку по id";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("== Просмотр заявки ==");
+        int currentId = input.askInt("Введите id заявки для просмотра заявки: ");
+        Item currentItem = tracker.findById(currentId);
+        if (currentItem != null) {
+            System.out.println(currentItem);
+        } else {
+            System.out.println("Такой заявки не существует");
+        }
+        return true;
+    }
+}
---
+++ src/main/java/ru/tracker/ReplaceAction.java
@@ -0,0 +1,22 @@
+package ru.tracker;
+
+public class ReplaceAction implements UserAction {
+    @Override
+    public String name() {
+        return "Изменить заявку";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("== Изменение заявки ==");
+        int id = input.askInt("Введите ID заявки: ");
+        String newName = input.askStr("Введите имя новой заявки: ");
+        Item newItem = new Item(newName);
+        if (tracker.replace(id, newItem)) {
+            System.out.println("Замена прошла успешно, " + "Актуальная заявка: " + newItem);
+        } else {
+            System.out.println("Введите корректный ID: ");
+        }
+        return true;
+    }
+}
