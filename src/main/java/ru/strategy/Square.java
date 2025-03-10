0518d140a6d92b46924761022ddae69db94d3015
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-10 23:32:46 +0300

    10.22 Шаблон проектирования, Стратегия

---
+++ src/main/java/ru/strategy/Square.java
@@ -0,0 +1,12 @@
+package ru.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    " _______" + ln
+                + "|       |" + ln
+                + "|       |" + ln
+                + "|_______|" + ln;
+    }
+}
