251d5e91bc97d930476afeb32e52755b58647d0b
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-11 16:38:50 +0300

    10.24 Тесты на StartUI

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -21,7 +21,8 @@ public class StartUI {
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new CreateAction(),
+        UserAction[] actions = {
+                new CreateAction(),
                 new FindAllAction(),
                 new ReplaceAction(),
                 new DeleteAction(),
