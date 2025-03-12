49520bcfeb91f3be5e05391f16d892e720eed322
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-12 20:29:54 +0300

    11.1 Исключение

---
+++ src/main/java/ru/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (int i = 0; i < shops.length; i++) {
+            System.out.println(shops[i]);
+        }
+    }
+}
