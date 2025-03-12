113c7cf9e0006131c1b726f20eb8c409144a4932
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-12 23:10:32 +0300

    11.2 NullPointerException

--- src/main/java/ru/tracker/UserStore.java
+++
@@ -1,9 +0,0 @@
-package ru.tracker;
-
-public class UserStore {
-    public static void main(String[] args) {
-        User user = new User(1, "root", "root");
-        String s = "Hello " + user;
-        System.out.println(s);
-    }
-}
