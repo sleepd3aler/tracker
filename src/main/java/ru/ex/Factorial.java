e5cf946842273d745bb4002b567ce73860bcf735
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-13 10:59:01 +0300

    11.4 Тестирование исключений

--- src/main/java/ru/ex/Factorial.java
+++ src/main/java/ru/ex/Factorial.java
@@ -1,19 +1,15 @@
 package ru.ex;
 
 public class Factorial {
-    public  int calc(int number) {
-        if (number < 1) {
-            throw new IllegalArgumentException("Число должно быть положительным и больше нуля");
-        }
+    public int calc(int number) {
         int result = 1;
-        for (int i = 1; i <= number; i++) {
+        for (int i = 2; i <= number; i++) {
             result *= i;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Factorial().calc(-0));
-
+        System.out.println(new Factorial().calc(5));
     }
 }
