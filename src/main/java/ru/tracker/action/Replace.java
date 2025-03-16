f7601bde86c06045bfa6dd75584ad7bbd4b5315a
sleepd3aler <enjoydeathcore228@gmail.com>
2025-03-16 15:57:44 +0300

    11.8 Обеспечить бесперебойную работы приложения Tracker

--- src/main/java/ru/tracker/action/Replace.java
+++ src/main/java/ru/tracker/action/Replace.java
@@ -20,13 +20,13 @@ public class Replace implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
         output.println("=== Редактирование заявки ===");
-        int id = input.askInt("Введите ID заявки: ");
-        String newName = input.askStr("Введите имя новой заявки: ");
+        int id = input.askInt("Введите ID заявки:");
+        String newName = input.askStr("Введите имя новой заявки:");
         Item newItem = new Item(newName);
         if (tracker.replace(id, newItem)) {
             output.println("Заявка изменена успешно.");
         } else {
-            output.println("Введите корректный ID: ");
+            output.println("Введите корректный ID:");
         }
         return true;
     }
--- src/main/java/ru/tracker/input/ValidateInput.java
+++ src/main/java/ru/tracker/input/ValidateInput.java
@@ -1,16 +1,12 @@
 package ru.tracker.input;
 
-import java.util.Scanner;
-
 public class ValidateInput extends ConsoleInput {
-    private Scanner scanner = new Scanner(System.in);
 
     @Override
     public int askInt(String question) throws NumberFormatException {
         do {
-            System.out.println(question);
             try {
-                return Integer.parseInt(scanner.nextLine());
+                return super.askInt(question);
             } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста введите корректные данные");
             }
