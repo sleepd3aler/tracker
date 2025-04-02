0b9d55e8a9d9183e78bd9aac757866a15618a55b
sleepd3aler <enjoydeathcore228@gmail.com>
2025-04-02 23:08:11 +0300

    13.5 Очередь с приоритетом

---
+++ src/main/java/ru/search/PriorityQueue.java
@@ -0,0 +1,25 @@
+package ru.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (task.getPriority() >= element.getPriority()) {
+                index++;
+            }
+            if (task.getPriority() < element.getPriority()) {
+                index = tasks.size() - 1;
+
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
