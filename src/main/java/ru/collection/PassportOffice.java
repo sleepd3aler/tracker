2e90a05b43f5baf045f23352232b189adafe9e0d
sleepd3aler <enjoydeathcore228@gmail.com>
2025-04-11 15:58:08 +0300

    16.2 Паспорт и жители

--- src/main/java/ru/collection/PassportOffice.java
+++ src/main/java/ru/collection/PassportOffice.java
@@ -15,6 +15,6 @@ public class PassportOffice {
     }
 
     public Citizen get(String passport) {
-        return citizens.getOrDefault(passport, null);
+        return citizens.get(passport);
     }
 }
