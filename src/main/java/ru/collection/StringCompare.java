97f8dda230dd0d7d5cdfd1414eeb38461397c44b
sleepd3aler <enjoydeathcore228@gmail.com>
2025-04-25 00:15:56 +0300

    17.4 Компаратор для строк

--- src/main/java/ru/collection/StringCompare.java
+++ src/main/java/ru/collection/StringCompare.java
@@ -6,17 +6,15 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int shortStringLength = Math.min(left.length(), right.length());
-        int result = Integer.compare(left.length(), right.length());
         for (int i = 0; i < shortStringLength; i++) {
             char leftChar = left.charAt(i);
             char rightChar = right.charAt(i);
-            result = Character.compare(leftChar, rightChar);
+            int result = Character.compare(leftChar, rightChar);
             if (result != 0) {
                 return result;
             }
-            result = Integer.compare(left.length(), right.length());
         }
-        return result;
+        return Integer.compare(left.length(), right.length());
     }
 
 }
