753c0c349eb44c9826ed940d6cbc19824305bdbe
sleepd3aler <enjoydeathcore228@gmail.com>
2025-04-25 19:22:15 +0300

    18.2 Что такое JavaDoc

--- src/main/java/ru/bank/Account.java
+++ src/main/java/ru/bank/Account.java
@@ -2,27 +2,66 @@ package ru.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает объект банковского аккаунта пользователя.
+ *
+ * @author Alexander Novitskiy
+ * @version 1.0
+ */
 public class Account {
+    /**
+     * Хранение реквизитов
+     */
     private String requisite;
+    /**
+     * Хранение текущего баланса
+     */
     private double balance;
 
+    /**
+     * Конструктор, с параметрами для создания объектов Account
+     *
+     * @param requisite - Реквизиты пользователя
+     * @param balance   - Текущий баланс пользователя
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет получить текущие реквизиты Аккаунта
+     *
+     * @return - возвращает реквизиты
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод позволяет получить текущий баланс Аккаунта
+     *
+     * @return - возвращает реквизиты
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод позволяет изменить текущее значение баланса Аккаунта
+     *
+     * @param balance - новое значение баланса
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод сравнивает Аккаунты по реквизитам
+     *
+     * @param o - объект с которым происходит сравнение
+     * @return - возвращает true, если объекты одинаковые, false - если разные
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -30,6 +69,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод сравнивает Аккаунты по хэш коду реквизитов
+     *
+     * @return - возвращает хэш код реквизитов
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(requisite);
--- src/main/java/ru/bank/User.java
+++ src/main/java/ru/bank/User.java
@@ -2,31 +2,71 @@ package ru.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает объекты пользователей банковского сервиса
+ *
+ * @author Alexander Novitskiy
+ * @version 1.0
+ */
 public class User {
+    /**
+     * Хранение паспорта
+     */
     private String passport;
+    /**
+     * Хранение имени пользователя
+     */
     private String username;
+
+    /**
+     * Конструктор с параметрами для создания объектов User
+     *
+     * @param passport - Паспорт пользователя
+     * @param username - Имя пользователя
+     */
 
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод, возвращает информацию о паспорте пользователя
+     * @return - возвращает информацию о паспорте польователя
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод, возвращает информацию о паспорте пользователя
+     * @return - возвращает информацию о паспорте пользователя
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод, возвращает информацию об имени пользователя
+     * @return - возвращает информацию об имени пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод, изменяет имя пользователя
+     * @param username имя пользователя
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Метод сравнивает объект пользователя по паспорту
+     * @param o - Объект пользователя
+     * @return - возвращает true, если объекты одинаковые, false - если разные
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -34,6 +74,11 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Метод сравнивает пользователй по хэш коду паспорта
+     *
+     * @return - возвращает хэш код паспорта
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(passport);
