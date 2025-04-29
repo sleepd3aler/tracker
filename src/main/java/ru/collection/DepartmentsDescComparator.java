8a8e2586be4a39109cb90be5c7a8faaeb753e64b
sleepd3aler <enjoydeathcore228@gmail.com>
2025-04-29 20:41:21 +0300

    19.1 Отсортировать департаменты

--- src/main/java/ru/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/collection/DepartmentsDescComparator.java
@@ -5,14 +5,9 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 0;
-        for (int i = 0; i < 2; i++) {
-            char leftChar = left.charAt(i);
-            char rightChar = right.charAt(i);
-            result = Character.compare(rightChar, leftChar);
-            if (leftChar != rightChar) {
-                return result;
-            }
+        boolean firstElems = left.charAt(0) == right.charAt(0) && left.charAt(1) == right.charAt(1);
+        int result = right.compareTo(left);
+        if (firstElems) {
             result = left.compareTo(right);
         }
         return result;
