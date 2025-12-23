220375609a339673cfdd8a1b6e87c887c6233536
sleepd3aler <enjoydeathcore228@gmail.com>
2025-12-23 21:42:17 +0300

    42.4 SQL схема проекта Tracker

--- db/001_ddl_create_items_table.sql
+++ db/001_ddl_create_items_table.sql
@@ -1,6 +1,6 @@
 create table items
 (
     id            serial primary key,
-    name          varchar(255),
-    creation_time timestamp
+    name          varchar(255) not null ,
+    creation_time timestamp not null
 );
